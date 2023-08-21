42db16a6c71c8d9a3d01c9e0778d3b38b28fa2c9
senachary <senachary1@gmail.com>
2023-08-21 22:31:53 +0100

    adds working on question functionality

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const jokeButton = document.getElementById("joke-button");
-const jokeList = document.getElementById("new-joke");
+const startButton = document.getElementById("start-button");
+const trueButton = document.getElementById("true-button");
+const falseButton = document.getElementById("false-button");
+const questionList = document.getElementById("new-question");
+const score = document.getElementById("quiz-score");
 
-async function fetchJoke() {
-  const response = await fetch("https://opentdb.com/api.php?amount=10", {
+
+
+//fetching question data
+async function fetchQuestion() {
+  const response = await fetch("https://opentdb.com/api.php?amount=10&type=boolean", {
     headers: {
       Accept: "application/json",
     },
@@ -10,11 +16,52 @@ async function fetchJoke() {
   return response.json();
 }
 
-//fetchJoke();
 
-async function clickButton() {
-  const data = await fetchJoke();
-  jokeList.textContent = data.results[0].question;
+
+//call the startQuiz function
+async function startQuiz() {
+    const data = await fetchQuestion();
+    let currentScore = 0;
+    let currentQuestion = 0;
+    score.textContent = currentScore
+    showQuestion()
 }
 
-jokeButton.addEventListener("click", clickButton);
+//Function to display the Questions
+function showQuestion() {
+    //determine if question number is less than question array length
+    if (currentQuestion < data.results.length) {
+        const question = data.results[currentQuestion].question;
+        const correctAnswer = data.results[currentQuestion].correct_answer;
+
+        questionList.textContent = question;
+        //user clicks true
+        trueButton.addEventListener('click', function () {
+            checkAnswer("True", correctAnswer)
+        });
+        //user clicks false
+        falseButton.addEventListener('click', function () {
+            checkAnswer("False", correctAnswer)
+        });
+
+    } else {
+        questionList.textContent = "Quiz Comepleted"
+    }
+    
+}
+//TODO
+//write checkAnswer function
+
+function checkAnswer(answerGiven, correctAnswer) {
+    
+}
+
+//start the quiz with start button
+startButton.addEventListener("click", startQuiz);
+
+
+/*Other functionality:
+Use category to display image
+Change to multichoice (A, B, C, D)
+Add more players (1-4)
+*/
